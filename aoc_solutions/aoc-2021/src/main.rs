928ba7447cf86352dfbc567f5e61bf08a5079e46
christhedevel <christhedevel@gmail.com>
2022-11-26 17:19:44 +0100

    add day 8

--- aoc_solutions/aoc-2021/src/main.rs
+++ aoc_solutions/aoc-2021/src/main.rs
@@ -9,6 +9,7 @@ mod day4;
 mod day5;
 mod day6;
 mod day7;
+mod day8;
 mod utils;
 
 fn main() {
@@ -39,7 +40,11 @@ fn main() {
     //crate::day6::solve_first(input.clone());
     //crate::day6::solve_second(input.clone());
 
-    let input = get_input(2021, 7);
+    // let input = get_input(2021, 7);
     // crate::day7::solve_first(input.clone());
-    crate::day7::solve_second(input.clone());
+    // crate::day7::solve_second(input.clone());
+
+    let input = get_input(2021, 8);
+    // crate::day8::solve_first(input.clone());
+    crate::day8::solve_second(input.clone());
 }
