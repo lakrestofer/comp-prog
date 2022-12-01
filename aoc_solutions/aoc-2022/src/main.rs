b8bf45f73e3446d56782f36ccf2bec1eac6d8fa9
christhedevel <christhedevel@gmail.com>
2022-12-01 18:13:16 +0100

    remove copied days from 2021

--- aoc_solutions/aoc-2022/src/main.rs
+++ aoc_solutions/aoc-2022/src/main.rs
@@ -3,16 +3,6 @@ extern crate dotenv_codegen;
 use utils::{get_input, input_string};
 
 mod day01;
-mod day02;
-mod day03;
-mod day04;
-mod day05;
-mod day06;
-mod day07;
-mod day08;
-mod day09;
-mod day10;
-mod day11;
 mod utils;
 
 fn main() {
